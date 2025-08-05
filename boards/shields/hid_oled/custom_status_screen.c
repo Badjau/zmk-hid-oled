1e3c1ae66981778e5b83ad8e547e624b1c8ee6a5
Badjau <franzvasquez4153@gmail.com>
2025-08-05 23:02:59 +0800

    HID_OLED name to defaults

--- boards/shields/hid_oled/custom_status_screen.c
+++ boards/shields/hid_oled/custom_status_screen.c
@@ -3,7 +3,7 @@
 #include <zephyr/logging/log.h>
 LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 
-#if IS_ENABLED(CONFIG_HID_OLED_WIDGET_STATUS)
+#if IS_ENABLED(CONFIG_NICE_VIEW_WIDGET_STATUS)
 static struct zmk_widget_screen screen_widget;
 #endif
 
@@ -11,7 +11,7 @@ lv_obj_t *zmk_display_status_screen() {
     lv_obj_t *screen;
     screen = lv_obj_create(NULL);
 
-#if IS_ENABLED(CONFIG_HID_OLED_WIDGET_STATUS)
+#if IS_ENABLED(CONFIG_NICE_VIEW_WIDGET_STATUS)
     zmk_widget_screen_init(&screen_widget, screen);
     lv_obj_align(zmk_widget_screen_obj(&screen_widget), LV_ALIGN_TOP_LEFT, 0, 0);
 #endif
